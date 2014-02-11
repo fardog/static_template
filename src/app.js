91b02224ea2c43c224fa2c99514038cd112b6780
nwittstock <nwittstock@north.com>
2014-02-11 09:30:03 -0800

    Simpler JS template.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ $(document).ready(function() {
 	// initialize Foundation
 	$(document).foundation();
 
+	
+	/***
+	 * viewModel: a Knockout view model for our page.
+	 */
 	var viewModel = function() {
 		var self = this;
 
 		self.header = ko.observable("Static Site Template");
-	};
+	}; /* end viewModel */
 
+
+	// Apply our Knockout bindings
 	var view = new viewModel();
-
 	ko.applyBindings(view);
 });
