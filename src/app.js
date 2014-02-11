754074dd37e3a302ee9d8b0ca72746fcd42ba687
nwittstock <nwittstock@north.com>
2014-02-10 23:52:32 -0800

    Include a very simple KO viewModel by default.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 
 	// initialize Foundation
 	$(document).foundation();
+
+	var viewModel = function() {
+		var self = this;
+
+		self.header = ko.observable("Static Site Template");
+	};
+
+	var view = new viewModel();
+
+	ko.applyBindings(view);
 });
