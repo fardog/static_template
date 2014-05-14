e67dec26c12ca05378424f10018a25cc5e439541
fardog <code@fardogllc.com>
2014-05-14 13:06:29 -0700

    now supports building for node-webkit

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,8 @@ module.exports = function (grunt) {
 			},
 			deploy: {
 				files: [
-					{expand: true, cwd: 'src/', src: ['img/*'], dest: 'www/assets/img/', filter: 'isFile'}
+					{expand: true, cwd: 'src/', src: ['img/*'], dest: 'www/assets/img/', filter: 'isFile'},
+					{expand: true, cwd: './', src: ['package.json'], dest: 'www/', filter: 'isFile'}
 				]
 			},
 		},
@@ -101,6 +102,16 @@ module.exports = function (grunt) {
 				livereload: true
 			}
 		},
+		nodewebkit: {
+	    options: {
+	        build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+	        mac: true, // We want to build it for mac
+	        win: false, // We want to build it for win
+	        linux32: false, // We don't need linux32
+	        linux64: false // We don't need linux64
+	    },
+	    src: ['./www/**/*'] // Your node-webkit app
+	  },
 		connect: {
 			all: {
 				options: {
@@ -119,6 +130,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
 	
 	
 	grunt.registerTask('default', config.build.default);
